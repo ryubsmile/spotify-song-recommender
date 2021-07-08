094a806750e0eed5574d68bbf0e824a55454dfbb
sophryu99 <sehwa_ryu@berkeley.edu>
2021-07-08 17:40:12 +0900

    chore: front end rendering

--- workspace/workspace/settings.py
+++ workspace/workspace/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+os.path.join(SETTINGS_PATH, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -29,8 +32,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    'recommender.apps.RecommenderConfig', # Added app name
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
